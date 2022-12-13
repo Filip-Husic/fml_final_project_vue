a693ab8d7a168b2f95580a263b92b4c568924002
Lana Pavić <lana.pavic@t.ht.hr>
2022-12-13 10:08:03 +0100

    Users!

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,10 +1,53 @@
 <template>
-  <h1>User list</h1>
+ <h1>User list</h1>
+ <table>
+    <thead>
+    <th>Email</th>
+    <th>First name</th>
+    <th>Is enabled </th>
+    <th>Last name</th>
+    <th>Password</th>
+    <th>Username</th>
+    </thead>
+    <tr v-for="user of users" :key="user.id">
+      <td>{{user.email}}</td>
+      <td>{{user.first_name}}</td>
+      <td>{{user.is_enabled}}</td>
+      <td>{{user.last_name}}</td>
+      <td>{{user.password}}</td>
+      <td>{{user.username}}</td>
+    </tr>
+    <tfoot>
+    <tr>
+      <td colspan="4">Total users: {{ users.length }}</td>
+    </tr>
+    </tfoot>
+  </table>
 </template>
-
 <script>
 export default {
-  name: "Users"
+  name: "Users",
+  data(){
+    return{
+      users: []
+    };
+  },
+  methods: {
+    async getUsers() {
+      try {
+
+        let response = await fetch("http://localhost:8080/api/user");
+        this.users = await response.json();
+        console.log(this.users);
+      } catch (error) {
+        console.log("Error = ", error);
+      }
+
+    }
+  },
+  created() {
+    this.getUsers();
+  }
 }
 </script>
 
