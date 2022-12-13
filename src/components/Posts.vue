8978ae755d1ad935f64fdc85a32c92a77514ef7d
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-12-13 10:09:56 +0100

    post table corrected

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,18 +1,20 @@
 <template>
 <h1>Post list</h1>
-  <table>
-    <thead>
+  <table class="table table-striped">
+    <thead class="table-dark">
     <th>Title</th>
     <th>Content</th>
     <th>Date</th>
     <th>Posted by</th>
     </thead>
+    <tbody>
     <tr v-for="post of posts" :key="post.id">
       <td>{{post.title}}</td>
       <td>{{post.content}}</td>
       <td>{{post.postedDate}}</td>
-      <td>{{post.postedBy}}</td>
+      <td>{{post.user.username}}</td>
     </tr>
+    </tbody>
     <tfoot>
     <tr>
       <td colspan="4">Total posts: {{ posts.length }}</td>
@@ -49,5 +51,35 @@ export default {
 </script>
 
 <style scoped>
+h1{
+  text-align: center;
+}
+.table {
+  border: 1px solid black;
+  border-collapse: collapse;
+  margin: auto;
+  width: 80%;
+  padding: 20px;
+}
 
+.table tr:nth-child(even) {
+  background-color: lightgray;
+}
+.table thead td:nth-child(even){
+  border-left: 1px solid white;
+  border-right: 1px solid white;
+}
+
+.table tbody th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+.table thead, tfoot {
+  background-color: black;
+  color: white;
+}
+.table tfoot {
+  text-align: center;
+}
 </style>
