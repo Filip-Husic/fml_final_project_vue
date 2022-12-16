0269677e3bb35053a87c1945138d5cddbbad8fcf
mbevki <bevki95matija@gmail.com>
2022-12-16 13:59:46 +0100

    HomeButton - added lodash for delaying display off the button... need to install lodash $ npm i -g npm $ npm i --save lodash

--- src/components/HomeButton.vue
+++ src/components/HomeButton.vue
@@ -1,64 +1,72 @@
 <script>
   import { defineComponent } from "vue";
+  import debounce from "lodash/debounce";
 
   export default defineComponent({
+    data() {
+      return {
+        handleDebouncedScroll: null,
+      };
+    },
     mounted() {
-      window.addEventListener("scroll", this.handleScroll);
+      // window.addEventListener("scroll", this.handleScroll);
+
+      this.handleDebouncedScroll = debounce(this.handleScroll, 300);
+      window.addEventListener("scroll", this.handleDebouncedScroll);
     },
 
     beforeUnmount() {
-      window.removeEventListener("scroll", this.handleScroll);
+      // window.removeEventListener("scroll", this.handleScroll);
+      window.removeEventListener("scroll", this.handleDebouncedScroll);
     },
 
     methods: {
       handleScroll() {
-        const scrollBtn = this.$refs.scrollTopButton;
+          const scrollBtn = this.$refs.scrollTopButton;
 
-        if (window.scrollY > 50) {
-          scrollBtn.classList.remove("invisible");
-        } else {
-          scrollBtn.classList.add("invisible");
-        }
+          if (window.scrollY > 50) {
+            scrollBtn.classList.remove("invisible");
+          } else {
+            scrollBtn.classList.add("invisible");
+          }
+        },
+      scrollToTop() {
+        window.scrollTo({ top: 0, behavior: "smooth" });
       },
-      // When the user clicks on the button, scroll to the top of the document
-      topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-      }
-    },
 
+    }
   });
 </script>
 
 <template>
-    <a href="#" ref="scrollTopButton">
-      <button class="myBtn" href="#">
-        ^
-      </button>
-    </a>
+  <!-- added class invisible so its invisible initialy -->
+  <button id="myBtn" ref="scrollTopButton" class="invisible" @click="scrollToTop">
+    ^
+  </button>
+
 </template>
 
-<style>
-  .myBtn {
+<style scoped>
+  #myBtn {
     /*display: none; !* Hidden by default *!*/
     position: fixed; /* Fixed/sticky position */
     bottom: 20px; /* Place the button at the bottom of the page */
     right: 30px; /* Place the button 30px from the right */
     z-index: 99; /* Make sure it does not overlap */
-    border: none; /* Remove borders */
-    outline: none; /* Remove outline */
-    background-color: red; /* Set a background color */
-    color: white; /* Text color */
-    cursor: pointer; /* Add a mouse pointer on hover */
-    padding: 15px; /* Some padding */
-    border-radius: 10px; /* Rounded corners */
-    font-size: 18px; /* Increase font size */
+    border: none;
+    outline: none;
+    background-color: red;
+    color: white;
+    cursor: pointer;
+    padding: 15px;
+    border-radius: 10px;
+    font-size: 18px;
     box-shadow: 0px 2px 10px black; /* Adds shadows around the button*/
-    transition: 500ms ease;
+    transition: 300ms ease;
   }
 
-  .myBtn:hover {
-    background-color: magenta; /* Add a dark-grey background on hover */
+  #myBtn:hover {
+    background-color: magenta;
     transition: 300ms ease-out;
   }
 </style>
