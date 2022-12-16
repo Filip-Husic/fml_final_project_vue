d4e9c139c28171ae5f88d963a1ca370c6c50b75c
mbevki <bevki95matija@gmail.com>
2022-12-16 12:28:13 +0100

    HomeButton - made some corrections, added transitions

--- src/components/HomeButton.vue
+++ src/components/HomeButton.vue
@@ -1,79 +1,64 @@
 <script>
-  export default {
-    data() {
-      return {}
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    mounted() {
+      window.addEventListener("scroll", this.handleScroll);
     },
+
+    beforeUnmount() {
+      window.removeEventListener("scroll", this.handleScroll);
+    },
+
     methods: {
-      button() {
-        let mybutton = document.getElementById("myBtn");
-        window.onscroll = function () {
-          scrollFunction()
+      handleScroll() {
+        const scrollBtn = this.$refs.scrollTopButton;
+
+        if (window.scrollY > 50) {
+          scrollBtn.classList.remove("invisible");
+        } else {
+          scrollBtn.classList.add("invisible");
         }
       },
-      scrollFunction(){
-        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-          mybutton.style.display="block";
-        }else {
-          mybutton.style.display = "none";
-        }
-      },
-      topFunction(){
+      // When the user clicks on the button, scroll to the top of the document
+      topFunction() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
       }
+    },
 
-    }
-  }
+  });
 </script>
 
 <template>
-<!--      <a href="#">-->
-<!--        <button-->
-<!--            type="button"-->
-<!--            class="btn btn-danger btn-floating btn-lg"-->
-<!--            id="btn-back-to-top">-->
-<!--          Back to top ^-->
-<!--        </button>-->
-<!--      </a>-->
-
-<!--      <a href="#">-->
-<!--        <button-->
-<!--            type="button"-->
-<!--            class="btn btn-danger btn-floating btn-lg"-->
-<!--            id="myBtn">-->
-<!--          ^-->
-<!--        </button>-->
-<!--      </a>-->
-
-    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+    <a href="#" ref="scrollTopButton">
+      <button class="myBtn" href="#">
+        ^
+      </button>
+    </a>
 </template>
 
-<style scoped>
-
-  /*#btn-back-to-top {*/
-  /*  position: fixed;*/
-  /*  bottom: 20px;*/
-  /*  right: 20px;*/
-  /*}*/
-  #myBtn {
-      display: none; /* Hidden by default */
-      position: fixed; /* Fixed/sticky position */
-      bottom: 20px; /* Place the button at the bottom of the page */
-      right: 30px; /* Place the button 30px from the right */
-      z-index: 99; /* Make sure it does not overlap */
-      border: none; /* Remove borders */
-      outline: none; /* Remove outline */
-      background-color: red; /* Set a background color */
-      color: white; /* Text color */
-      cursor: pointer; /* Add a mouse pointer on hover */
-      padding: 15px; /* Some padding */
-      border-radius: 10px; /* Rounded corners */
-      font-size: 18px; /* Increase font size */
+<style>
+  .myBtn {
+    /*display: none; !* Hidden by default *!*/
+    position: fixed; /* Fixed/sticky position */
+    bottom: 20px; /* Place the button at the bottom of the page */
+    right: 30px; /* Place the button 30px from the right */
+    z-index: 99; /* Make sure it does not overlap */
+    border: none; /* Remove borders */
+    outline: none; /* Remove outline */
+    background-color: red; /* Set a background color */
+    color: white; /* Text color */
+    cursor: pointer; /* Add a mouse pointer on hover */
+    padding: 15px; /* Some padding */
+    border-radius: 10px; /* Rounded corners */
+    font-size: 18px; /* Increase font size */
+    box-shadow: 0px 2px 10px black; /* Adds shadows around the button*/
+    transition: 500ms ease;
   }
 
-  #myBtn:hover {
+  .myBtn:hover {
     background-color: magenta; /* Add a dark-grey background on hover */
-    transition: 500ms ease-out;
+    transition: 300ms ease-out;
   }
-
 </style>
