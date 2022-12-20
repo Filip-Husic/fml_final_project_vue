c9ca29c3723d5cd7b3ad72d3cf6bbd7dcf70f511
mbevki <bevki95matija@gmail.com>
2022-12-20 15:45:15 +0100

    TopMenu & Footer - changed the button for About us, it's now in the TopMenu

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <p class=""><a href="https://www.hrvatskitelekom.hr/" target="_blank"><img src="src/assets/img/telekom-logo-claim.svg" alt="Telekom - Erleben, was verbindet." loading="lazy"></a>&copy; 2022 Lana Pavić, Matija Bevandić & Filip Husić </p>
-      <nav class="nav nav-pills">
-        <ul class="nav me-auto">
-          <li>
-            <router-link :to="{name:'About'}"><button class="btn">About us</button></router-link>
-          </li>
-        </ul>
-      </nav>
+<!--      <nav class="nav nav-pills">-->
+<!--        <ul class="nav me-auto">-->
+<!--          <li>-->
+<!--            <router-link :to="{name:'About'}"><button class="btn">About us</button></router-link>-->
+<!--          </li>-->
+<!--        </ul>-->
+<!--      </nav>-->
     </footer>
   </div>
 </template>
