e54cba07efa350e5b525c296d74507a6969899fd
mbevki <bevki95matija@gmail.com>
2022-12-21 21:16:23 +0100

    Logout - made CSS for it, added "Cancel" button

--- src/components/LogoutButton.vue
+++ src/components/LogoutButton.vue
@@ -17,9 +17,58 @@ export default defineComponent({
 </script>
 
 <template>
-  <p>Are you sure that you want to logout?</p>
-  <button @click="logout" v-if="authStore.isAuthenticated">Logout</button>
+  <article>
+    <p>Are you sure that you want to logout?</p>
+
+    <button @click="logout" v-if="authStore.isAuthenticated" class="btnOut">Logout</button>
+    <router-link class="nav-link active" to="/"><button class="btnHome">Cancel</button></router-link>
+
+  </article>
 </template>
 <style scoped>
+  article{
+    padding: 55px;
+    width: 300px;
+    height: 200px;
+    margin: 20px auto;
+    background-color: lightgrey;
+    border-radius: 20px;
+  }
+
+  .btnOut{
+    width: 120px;
+    border-radius: 10px;
+    border-width: 0;
+    background: #f05f40;
+    color: lightgrey;
+    box-shadow: 0 2px 5px #000000;
+    padding: 1px;
+    margin: auto;
+  }
+  .btnOut:hover{
+    background-color: lightgrey;
+    color: #f05f40;
+    transition: 300ms ease-out;
+  }
+
+  .btnHome{
+    width: 120px;
+    border-radius: 10px;
+    border-width: 0;
+    background: lightgrey;
+    color: #f05f40;
+    box-shadow: 0 2px 5px #000000;
+    padding: 1px;
+    margin-top: 10px;
+
+  }
+  .btnHome:hover{
+    background-color: #f05f40;
+    color: lightgrey;
+    transition: 300ms ease-out;
+  }
+
+
+
 
 </style>
